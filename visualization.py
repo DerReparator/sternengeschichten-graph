cc80f533dc05cd2496bde39d0d0c63f995f6caa8
Philipp Dormeier <30900810+DerReparator@users.noreply.github.com>
2021-12-13 22:41:00 +0100

    added visualization for unconnected episodes improved layouting of Graphviz document
    Signed-off-by: Philipp Dormeier <30900810+DerReparator@users.noreply.github.com>

--- visualization.py
+++ visualization.py
@@ -3,17 +3,45 @@ import os
 from typing import List
 
 LINK_FILE_DIRECTORY = 'cache'
+GRAPH_NAME = 'sternengeschichten'
 
 def exportGraph():
-    g = graphviz.Digraph('G', filename='sternengeschichten.gv')
-    for linksFile in findAllLinksFiles(LINK_FILE_DIRECTORY):
-        filename, file_extension = os.path.splitext(os.path.basename(linksFile))
-        filename = filename.replace("Episode", "").replace(".links", "")
-        fromID = int(filename)
-        for toID in getDestinationEpisodesFromLinksFile(linksFile):
-            g.edge(str(fromID), str(toID))
+    g = graphviz.Digraph('G',
+    filename=GRAPH_NAME+'.gv',
+    engine='fdp',
+    strict=True,
+    graph_attr={'overlap': 'prism', 'overlap_scaling': '-3.5'}
+    )
 
+    # Episodes without a reference to another episode
+    standaloneEpisodes = set()
+    overallMentionedEpisodes = set()
+    for linksFile in findAllLinksFiles(LINK_FILE_DIRECTORY):
+        fromID = parseLinksFilenameForEpisodeID(linksFile)
+        mentionedEpisodes = getDestinationEpisodesFromLinksFile(linksFile)
+        if len(mentionedEpisodes) == 0:
+            standaloneEpisodes.add(fromID)
+        for toID in mentionedEpisodes:
+            g.edge(str(fromID), str(toID))
+            overallMentionedEpisodes.add(fromID)
+            overallMentionedEpisodes.add(toID)
+
+    # Remove any episodes that are connected to at least 1 other episode in any way
+    standaloneEpisodes.difference_update(overallMentionedEpisodes)
+    handleStandaloneEpisodes(standaloneEpisodes, g)
     g.view()
+
+def parseLinksFilenameForEpisodeID(linksFilename: str) -> int:
+    filename, file_extension = os.path.splitext(os.path.basename(linksFilename))
+    filename = filename.replace("Episode", "").replace(".links", "")
+    return int(filename)
+
+def handleStandaloneEpisodes(standaloneEpisodes, graph: graphviz.Digraph):
+    with graph.subgraph(name=f'cluster_{GRAPH_NAME}_0') as c:
+        c.attr(style='filled', color='lightgrey')
+        for epId in [str(ep) for ep in standaloneEpisodes]:
+            c.node(epId)
+        c.attr(label='Standalone Episodes')
 
 def getDestinationEpisodesFromLinksFile(pathToLinksFile: str) -> List[int]:
     with open(pathToLinksFile, 'r') as f:
